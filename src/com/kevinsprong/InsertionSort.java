ba55233207670a80c991a476670ec2351f248744
Kevin Sprong <kevinsprong@gmail.com>
2014-09-18 22:25:43 -0400

    blabbity blah

--- src/com/kevinsprong/InsertionSort.java
+++ src/com/kevinsprong/InsertionSort.java
@@ -1,2 +1,19 @@
 package com.kevinsprong;
 
+import java.util.Arrays;
+
+public class InsertionSort {
+	
+	private static void insertionSort(int[] array) {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		int[] array = {1,4,6,2,5,8,9,25,3,9};
+		
+		System.out.println(Arrays.toString(array));
+		insertionSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+}
