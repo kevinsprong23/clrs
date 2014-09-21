9222d7d7df677c419d98c38ba0474986ec9fe5a9
Kevin Sprong <kevinsprong@gmail.com>
2014-09-21 00:19:25 -0400

    insertion inshmertion

--- src/com/kevinsprong/InsertionSort.java
+++ src/com/kevinsprong/InsertionSort.java
@@ -6,7 +6,29 @@ import java.util.Random;
 public class InsertionSort {
 	
 	private static void insertionSort(int[] array) {
-		
+		// loop indices off by 1; Cormen index is 1:length
+		for (int j = 1; j < array.length; j++) {
+			int key = array[j];
+			int i = j - 1;
+			while (i >= 0 && array[i] > key) {
+				array[i+1] = array[i];
+				i -= 1;
+			}
+			array[i+1] = key;
+		}
+	}
+	
+	private static void insertionSortDec(int[] array) {
+		// loop indices off by 1; Cormen index is 1:length
+		for (int j = 1; j < array.length; j++) {
+			int key = array[j];
+			int i = j - 1;
+			while (i >= 0 && array[i] < key) {
+				array[i+1] = array[i];
+				i -= 1;
+			}
+			array[i+1] = key;
+		}
 	}
 	
 	private static void fillWithRandomInts(int[] array) {
@@ -17,11 +39,13 @@ public class InsertionSort {
 	}
 	
 	public static void main(String[] args) {
-		int[] array = new int[50];
+		int[] array = new int[20];
 		fillWithRandomInts(array);
 		
 		System.out.println(Arrays.toString(array));
 		insertionSort(array);
+		System.out.println(Arrays.toString(array));
+		insertionSortDec(array);
 		System.out.println(Arrays.toString(array));
 	}
 }
