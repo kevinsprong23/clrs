a41bbd691a993101d2916fad49f10d1662bfb911
Kevin Sprong <kevinsprong@gmail.com>
2014-10-05 21:42:07 -0400

    lg function in util, plus moreeeeee randoommmmmm

--- src/com/kevinsprong/Exercise5_1_2.java
+++ src/com/kevinsprong/Exercise5_1_2.java
@@ -9,29 +9,22 @@ public class Exercise5_1_2 {
 		Random rand = new Random();
 		// use rand.nextInt(2) as Random(0, 1) function in CLRS
 		
-		// I think we can binary search this
-		// start with range [a, b], call random(0,1)
-		// if 0, search bottom half, if 1, search top half
-		int updown, mid, rounddir;
-		while (a < b) {
-			updown = rand.nextInt(2);
-			mid = (b + a) / 2;
-			
-			// if odd, need to see which way to count middle element
-			rounddir = ((b - a) % 2 == 1) ? rand.nextInt(2) : 0;
-			
-			// iterative binary search, to keep same Random() instance
-			if (updown == 0) {
-				b = mid - rounddir;
-			} else {
-				a = mid + 1 - rounddir;
+		// find number of bits we need to generate
+		int n = b - a;
+		int c = (int) Math.ceil(util.lg(n)) + 1;  // 2^c may be bigger than n
+		
+		int csum = Integer.MAX_VALUE;
+		while (csum > n) {
+			csum = 0;
+			for (int i = c - 1; i >= 0; i--) {  // horner's method
+				csum = rand.nextInt(2) + 2 * csum;
 			}
 		}
-		return a;
+		return csum + a;
 	}
 
 	public static void main(String[] args) {
-		int[] randArray = new int[10];
+		int[] randArray = new int[300];
 		for (int i = 0; i < randArray.length; i++) {
 			randArray[i] = random(1, 3);
 		}
