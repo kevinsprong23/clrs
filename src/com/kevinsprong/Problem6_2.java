da068e69aaa735c37fe5ebb0eee8f4685ae17955
Kevin Sprong <kevinsprong@gmail.com>
2014-10-11 18:53:14 -0400

    dary heap son

---
+++ src/com/kevinsprong/Problem6_2.java
@@ -0,0 +1,13 @@
+package com.kevinsprong;
+
+public class Problem6_2 {
+	// analysis of D-ary heaps
+	 
+	// a. How would you represent a d-ary heap in an array?
+	// children of index i are at d*i + 1...d*i + 2..d*(i + 1)
+	
+	// b. What is the height of a d-ary heap of n elements?
+	// floor ( log d of n)
+	
+
+}
